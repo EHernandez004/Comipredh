9f7bbcf20a334310a0ee00e05709728b1fb42780
Pamela <pamelabustos630@gmail.com>
2020-08-07 20:07:10 -0500

    Formularios Usuario

---
+++ resources/views/admin/user/create.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.app')
+
+@section('title','Crear Usuario')
+@section('body-class','product-page')
+
+@section('content')
+<div class="header header-filter" style="background-image: url('{{asset('img/img.jpg')}}');">
+</div>
+
+<div class="main main-raised">
+    <div class="container">
+        <div class="section text-center">
+            <h2 class="title text-center">Agregar Usuario</h2>
+
+<!--            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+-->
+
+            <form method="post" action="{{ url('') }}">    
+                {{ csrf_field() }}
+
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="form-group label-floating">
+                            <label class="control-label">Nombre</label>
+                            <input type="text" class="form-control" name="name">
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4">
+                        <div class="form-group label-floating">
+                            <label class="control-label">Apellidos</label>
+                            <input type="text" class="form-control" name="apellidos">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="form-group label-floating">
+                            <label class="control-label">Telefono</label>
+                            <input type="text" class="form-control" name="telefono" >
+                        </div>
+                    </div>
+
+                     <div class="col-sm-4">
+                        <div class="form-group label-floating">
+                            <label class="control-label">Rol</label>
+                            <select class="form-control" name="rol">
+                                <option value="0">No</option>
+                                <option value="1">si</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4">
+                        <div class="form-group label-floating">
+                            <label class="control-label">Correo</label>
+                            <input type="email" class="form-control" name="email">
+                        </div>
+                    </div>
+                      <div class="col-sm-4">
+                        <div class="form-group label-floating">
+                            <label class="control-label">Contraseña</label>
+                            <input type="password" class="form-control" name="password">
+                        </div>
+                    </div>
+                
+                    
+
+                <br><br><br><br><br><br><br><br>
+
+                    <button class="btn btn-info">Agregar</button>
+                    <a href="{{ url('/admin/empresa') }}" class="btn btn-default">Cancelar</a>
+                </form>    
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<footer class="footer">
+    <div class="container">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="">
+                        &copy; Derechos Reservados por COMIPREDH AC.
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="collapse navbar-collapse" id="navigation-example">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href=""> Nuestras Redes Sociales
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+    </div>
+</footer>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,69 +1,99 @@
 @extends('layouts.app')
 
-@section('title','Bienvenido')
-@section('body-class','landing-page')
+@section('title','Administradores')
+@section('body-class','product-page')
 
 @section('content')
-<div class="wrapper">
-        <div class="header header-filter" style="background-image: url('{{asset('img/img.jpg')}}');">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                  <h1 class="title">Buscar Trabajo Nunca fue más Fácil</h1>
-                        
-                        <div class="col-12 col-md-9 mb-2 mb-md-0">
-                            <input type="buscador" class="form-control form-control-lg" placeholder="Buscar Puesto ó Área">
-                        </div>
-                        <div class="col-12 col-md-3">
-                            <button type="buscar" class=" btn-simple btn-primary btn-lg">Buscar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>      
-    </div>
-    
-    
-    <footer class="footer">
-        <div class="container">
-            <nav class="pull-left">
-                <ul>
-                    <li>
-                        <a href="">
-                            &copy; Derechos Reservados por COMIPREDH AC.
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="collapse navbar-collapse" id="navigation-example">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href=""> Nuestras Redes Sociales
-                        </a>
-                    </li>
-                    <li>
-                        <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-                            <i class="fa fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-                            <i class="fa fa-facebook-square"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-                            <i class="fa fa-instagram"></i>
-                        </a>
-        </div>
-    </footer>
+<div class="header header-filter" style="background-image: url('{{asset('img/img.jpg')}}');">
 </div>
+
+<div class="main main-raised">
+    <div class="container">
+        <div class="section text-center">
+            <h2 class="title">Administradores</h2>
+
+            <div class="team">
+                <div class="row">
+                    <a href="">Lista de Administradores</a>
+
+                    <table class = "table">
+                        <thead>
+                            <tr>
+                                <th class="text-center"> ID_Administrador </th>
+                                <th class="col-md-2 text-center"> Nombre </th>
+                                <th class="col-md-2 text-center"> Apellidos </th>
+                                <th class="col-md-2 text-center"> Telefono </th>
+                                <th class="col-md-2 text-center"> Correo </th>
+                                <th class="col-md-2 text-center"> Acciones </th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td class="text-center"> {{$user->id}} </td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->apellidos}}</td>
+                                <td>{{$user->telefono}}</td>
+                                <td>{{$user->email}}</td>
+                                <td class = "td-actions text-center">
+                                    
+                                    <form method="post" action="{{ url('/admin/user/'.$user->id)}}">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                        <a href="{{ url('/admin/user/'.$user->id.'/edit') }}"  rel="tooltip" title="Editar Acceso" class="btn btn-success btn-simple btn-xs">
+                                            <i class = "fa fa-edit"></i>
+                                        </a>
+                                        <button type="submit" rel="tooltip" title = "Eliminar" class = "btn btn-danger btn-simple btn-xs">
+                                            <i class = "fa fa-times"></i>
+                                        </button>
+                                    </form>
+
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    
+                </div>
+            </div>        
+        </div>
+    </div>
+</div>
+
+
+
+<footer class="footer">
+    <div class="container">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="">
+                        &copy; Derechos Reservados por COMIPREDH AC.
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="collapse navbar-collapse" id="navigation-example">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href=""> Nuestras Redes Sociales
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+    </div>
+</footer>
 @endsection
